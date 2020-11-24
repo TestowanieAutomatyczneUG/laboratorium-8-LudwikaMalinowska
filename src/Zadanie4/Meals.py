728d12adeb2e1e3952adb3f882e562841868a5b5
Ludwika Malinowska <malinowska.L@wp.pl>
2020-11-24 22:47:36 +0100

    Zadanie 4 - dodano test sprawdzajacy poprawne zwrocenie linku do zdjecia potrawy

--- src/Zadanie4/Meals.py
+++ src/Zadanie4/Meals.py
@@ -26,4 +26,12 @@ class Meals:
             meal = requests.get(link + name)
             return meal.json()['meals'][0]['strInstructions']
 
+    def get_meal_picture(self, name):
+        if not isinstance(name, str):
+            raise TypeError
+        else:
+            link = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+            meal = requests.get(link + name)
+            return meal.json()['meals'][0]['strMealThumb']
+
 
--- tests/Zadanie4_tests/test_Meals.py
+++ tests/Zadanie4_tests/test_Meals.py
@@ -11,6 +11,11 @@ class TestMeal(unittest.TestCase):
         instructions = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm."
         self.assertEqual(self.temp.get_instructions('Arrabiata'), instructions)
 
+    def test_get_meal_picture(self):
+        pictureLink = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
+        self.assertEqual(self.temp.get_meal_picture('Arrabiata'), pictureLink)
+
+
     def tearDown(self):
         self.temp = None
 
